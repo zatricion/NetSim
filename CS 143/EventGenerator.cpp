47894f878cc2b2d13ed58b94cdcd906f7f95c2d5
zatricion <zatricion@gmail.com>
2013-11-02 20:27:24 -0700

    Links, Devices

--- CS 143/EventGenerator.cpp
+++ CS 143/EventGenerator.cpp
@@ -2,25 +2,50 @@
 //  CS 143
 //
 //  Event Generator
+//  * Link
 //  * Device
 //   - Host
 //   - Router
-//  * Link
+
 
 #include "EventGenerator.h"
 #include "Event.h" // TODO: Write event class
+#include "Packet.h" // TODO: Write packet class
 #include <queue>
 
-class EventGen
+class EventGenerator
 {
 protected:
-    std::priority_queue<int, std::vector<int>, std::greater<int> > eventHeap;
+    std::priority_queue<Event, std::vector<Event>, std::greater<Event> > eventHeap;
+    // TODO: override operator> for event class
+    
 public:
-    virtual ~EventGen();
+    virtual ~EventGenerator();
     
     std::string getId() const;
-    virtual 
+
     virtual void addEvent() = 0;
     virtual Event getEvent() = 0;
     
 };
+
+class Link : public EventGenerator
+{
+private:
+    std::queue<Packet>
+    
+};
+
+class Device : public EventGenerator
+{
+public:
+    void addEvent() {
+        
+    }
+    
+    Event getEvent() {
+        
+    }
+    
+};
+
