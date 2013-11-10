c5379ad44647621a95ca3ce51f24499e5f716d8e
zatricion <zatricion@gmail.com>
2013-11-09 19:16:35 -0800

    filled out FlowGenerator class

--- CS_143/FlowGenerator.cpp
+++ CS_143/FlowGenerator.cpp
@@ -1,9 +1,19 @@
-//
-//  FlowGenerator.cpp
-//  CS 143
-//
-//  Created by Michael Lauria on 11/9/13.
-//  Copyright (c) 2013 Anish. All rights reserved.
-//
-
 #include "FlowGenerator.h"
+
+FlowGenerator::FlowGenerator(std::vector<std::tuple<std::string, std::string, int, float>> flows)
+{
+    // Go through flows, create FlowEvents, push onto eventHeap
+    for (std::vector<std::tuple>::iterator it = flows.begin();
+         it != flows.end();
+         it++)
+    {
+        // unpack flow
+        std::tie (source, dest, size, time) = *it;
+        
+        // create FlowEvent
+        flow = FlowEvent(source, dest, size, time);
+        
+        // add event to eventHeap
+        eventHeap.push(flow);
+    }
+}
--- CS_143/FlowGenerator.h
+++ CS_143/FlowGenerator.h
@@ -1,14 +1,16 @@
-//
-//  FlowGenerator.h
-//  CS 143
-//
-//  Created by Michael Lauria on 11/9/13.
-//  Copyright (c) 2013 Anish. All rights reserved.
-//
-
 #ifndef __CS_143__FlowGenerator__
 #define __CS_143__FlowGenerator__
 
 #include <iostream>
+#include <tuple>
+#include "EventGenerator.h"
+
+class FlowGenerator : EventGenerator
+{
+public:
+    // Constructor takes a vector of flows and add FlowEvents to the eventHeap
+    // Flow format: (source, destination, size, start time)
+    FlowGenerator::FlowGenerator(std::vector<std::tuple<std::string, std::string, int, float>> flows);
+};
 
 #endif /* defined(__CS_143__FlowGenerator__) */
