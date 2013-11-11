9aaa2dd8557c01a41908b8a45504ce1f63f25083
aagarwal1990 <aagarwal@caltech.edu>
2013-11-11 00:29:48 -0800

    bigfixes unack event

--- CS_143/Host.cpp
+++ CS_143/Host.cpp
@@ -83,7 +83,6 @@ void Host::giveEvent(std::unique_ptr<PacketEvent> new_event)
         Packet new_packet = new_event->packet;
         std::string source = new_event->source;
         float now = new_event->eventTime();
-        
     }
 }
 
