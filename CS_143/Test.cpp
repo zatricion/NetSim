807ae4abc4e5517a67fbd2bdb33be0867a65f78f
zatricion <zatricion@gmail.com>
2013-11-12 16:43:18 -0800

    adding full simulation test

--- CS_143/Test.cpp
+++ CS_143/Test.cpp
@@ -5,7 +5,8 @@
 // @MaxHorton TODO:
 // This method will contain unit tests for each of the objects.
 // It will be implemented as the network objects are created.
-int main() {
+int main()
+{
     std::cout << "Preparing to test objects." << std::endl;
     std::cout << "Testing Object constructors." << std::endl;
     packetTest();
@@ -16,7 +17,8 @@ int main() {
 /**
  * Simple test of packets.  Not much to test...
  */
-void packetTest() {
+void packetTest()
+{
 	std::cout << "Testing packets...";
 	//Packet p("uuid", "final_dest", "source", 10, true, false, false, NULL, 0);
 	// Not really anything to test... no methods in Packet.h
@@ -27,10 +29,36 @@ void packetTest() {
 /**
  * Simple test of events.
  */
-void eventTest() {
+void eventTest()
+{
 	std::cout << "Testing events...";
 	Event e("destination", "source", 1.5);
 	Event e2("destination", "source", 1.8);
 	assert (e2 > e);
 	std::cout << "Events passed tests!" << std::endl;
+}
+
+/*
+ * Test a simulation
+ */
+
+void simTest0()
+{
+    // test a simulation with just two hosts, one link, one flow
+    Handler handler = Handler();
+    
+    // add link1
+    Link link1 = Link((64 * 8 * 1000.0), 0.01, pow(10, 7), "host1", "host2",
+                      "link1");
+    
+    // create congestion algorithm
+    ccAlg =
+    
+    // add flow
+    Flow flow1 = Flow("flow1", "host1", "host2", ccAlg,
+                      // TODO: enter data in bits, fix Flow class
+    
+    // add host1
+    Host host1 = Host(
+    handler.addGenerator(<#std::unique_ptr<EventGenerator> gen#>)
 }
--- CS_143/Test.h
+++ CS_143/Test.h
@@ -1,8 +1,21 @@
 #include <iostream>
 #include "Packet.h"
+#include "Flow.h"
 #include "Event.h"
+#include "UnackEvent.h"
+#include "FlowEvent.h"
+#include "PacketEvent.h"
+#include "EventGenerator.h"
+#include "FlowGenerator.h"
+#include "Router.h"
+#include "Host.h"
+#include "Link.h"
+#include "Handler.h"
+#include "CongestionAlg.h"
 #include <cassert>
+#include <cmath>
 
 int main();
 void packetTest();
 void eventTest();
+void simTest0();
