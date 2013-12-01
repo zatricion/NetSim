122f04ab9165c713a1132d696e03dee731933b54
Alex Jose <ajose@caltech.edu>
2013-12-01 02:43:04 -0800

    trash

--- CS_143/src/IO/Plotter.cpp
+++ CS_143/src/IO/Plotter.cpp
@@ -1,12 +1,11 @@
 #include "Plotter.h"
-#include "gnuplot-iostream.h"
+//#include "gnuplot-iostream.h"
 
 Plotter sim_plotter = Plotter();
 
 void Plotter::logLinkRate(std::string name,
                           std::tuple<float, float> rate_data) {
     link_rate[name].push_back(rate_data);
-    
 }
 
 void Plotter::plot(plot_data data) {
