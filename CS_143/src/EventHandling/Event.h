a7ffaab933f1ed1221d3805da87a090e8847bd5a
Maxwell Horton <Maxwell.Christian.Horton@gmail.com>
2013-12-10 12:20:13 -0800

    Commented more methods.

--- CS_143/src/EventHandling/Event.h
+++ CS_143/src/EventHandling/Event.h
@@ -1,23 +1,34 @@
+/**
+ * Abstract class representing a generic Event in our Event based simulation.
+ */
 #ifndef __CS_143__Event__
 #define __CS_143__Event__
 
+
 #include <string>
 
-class Event
-{
+
+class Event {
+
 public:
-    Event(std::string dest, std::string src, double ts);
-    
-    double eventTime() const;
-        
+    // Fields
     std::string destination;
     std::string source;
+
+    // Constructor
+    Event(std::string dest, std::string src, double ts);
     
+    // Accessors
+    double eventTime() const;
+    // These are marked as virtual because we want to override them.
+    // But they are implemented to give a framework of how they should look.
     virtual std::string toString();
     virtual std::string getType();
     
 protected:
+    // Fields
     double timestamp;
 };
+
 
 #endif /* defined(__CS_143__Event__) */
--- CS_143/src/EventHandling/FlowEvent.h
+++ CS_143/src/EventHandling/FlowEvent.h
@@ -1,8 +1,16 @@
+/** 
+ * A FlowEvent contains a Flow, and lives in the FlowGenerator's eventHeap
+ * until the Flow should start.  Then, the FlowEvent passes the flow to the
+ * correct Host.
+ */
+
+
 #ifndef __CS_143__FlowEvent__
 #define __CS_143__FlowEvent__
 
 
 class Host;
+
 
 #include "../EventGenerators/EventGenerator.h"
 #include "Event.h"
@@ -10,18 +18,23 @@ class Host;
 #include "../EventGenerators/Flow.h"
 #include <string>
 
+
 class FlowEvent : public Event {
 public:
+    // Fields
+    int data_size;
+    std::shared_ptr<Flow> flow;
+
+    // Constructor
     FlowEvent(std::shared_ptr<Flow> flowobj,
               std::string dest,
               std::string src,
               double ts);
 
-    int data_size;
-    std::shared_ptr<Flow> floww;
-    
+    // Accessors
     virtual std::string toString() override;
     virtual std::string getType() override;
 };
+
 
 #endif /* defined(__CS_143__FlowEvent__) */
