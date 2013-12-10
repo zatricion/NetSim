2b083dc8bbd873525f47088b65940aa7f1bb4845
zatricion <zatricion@gmail.com>
2013-12-10 12:50:39 -0800

    Path.h comments

--- CS_143/src/EventGenerators/Path.h
+++ CS_143/src/EventGenerators/Path.h
@@ -18,19 +18,26 @@ public:
     
     // Copy Constructor
     Path(const Path& other);
-        
+    
+    // Adds a link to the path
     void addLink(std::string link, double delay, double ts);
     
+    // Gets total delay along the path
     double getTotalDelay() const;
     
+    // Updates a link weight
     void updateLinkWeight(std::string link, double delay, double time);
     
+    // Updates all link weights given another path
     void updateAll(const Path& other);
     
+    // Checks a path for a link
     bool hasCycle(std::string link);
     
+    // Gets the next link in the path
     std::string getNextLink() const;
     
+    // Returns a string representation of a path
     std::string toString();
 };
 
