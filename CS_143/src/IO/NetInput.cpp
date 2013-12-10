8f9275cc7980b09ea0619c80f460b21f67643937
Alex B. Jose <ajose@caltech.edu>
2013-12-10 15:40:07 -0800

    update IO commenting

--- CS_143/src/IO/NetInput.cpp
+++ CS_143/src/IO/NetInput.cpp
@@ -1,5 +1,13 @@
 #include "NetInput.h"
 
+/**
+ * Run the parser on a network description and return 
+ * a reference to the resulting handler.
+ *
+ * @param file an ifstream to read input from.
+ *
+ * @return reference to a handler containing the necessary network objects.
+ */
 Handler& read_input(std::ifstream file) {
   return parse(file);
 }
